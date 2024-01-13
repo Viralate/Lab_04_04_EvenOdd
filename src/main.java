b531ff59e5bfff820e500e566d84b3bc53aeb929
Viralate <dylan.geraci1@gmail.com>
2024-01-13 14:03:23 -0500

    Initial commit

---
+++ src/main.java
@@ -0,0 +1,14 @@
+public class main {
+    public static void main(String[] args) {
+        //declare variables
+        int numToExamine = 2;
+
+        if (numToExamine % 2 == 0 ) {
+            System.out.println(numToExamine + " is Even.");
+            
+        } else {
+            System.out.println(numToExamine + " is Odd.");
+        }
+        
+    }
+}
